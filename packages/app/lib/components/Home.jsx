ba800497d58cf2aace05b93313b8d30ec6dbb87e
Jordan Higgins <jordandhiggins@gmail.com>
2021-08-27 16:34:13 -0600

    Add modal library

--- packages/app/lib/components/Home.jsx
+++ packages/app/lib/components/Home.jsx
@@ -1,8 +1,32 @@
 import React, { useEffect, useState } from 'react';
 import { Pokemon } from '../modules/pokemon';
+import SearchModal from './SearchModal';
+
+import Modal from 'react-modal';
+
+const SearchButton = () => {
+  return(
+    <button>Search</button>
+  );
+}
+
 
 const Home = () => {
-  [pokemon, setPokemon] = useState("pikachu");
+  const [pokemon, setPokemon] = useState("pikachu");
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    subtitle.style.color = '#f00';
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
 
   useEffect(() => {
     Pokemon.card.find('base1-4')
@@ -13,7 +37,15 @@ const Home = () => {
   
   return(
     <div>
-      { pokemon }
+      <button onClick={openModal}>Open Modal</button>
+      <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        contentLabel="Example Modal"
+      >
+        <SearchModal />
+      </Modal>
     </div>
   );
 }
---
+++ packages/app/lib/components/SearchModal.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Components } from 'meteor/vulcan:core';
+
+const SearchModal = () => {
+  return(
+    <div>
+        SEARCH FOR POKEMON
+    </div>
+  );
+}
+
+export default SearchModal;
