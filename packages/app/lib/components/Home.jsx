9347e5e6f7588e398399ef851a7cd3a139db540c
Jordan Higgins <jordandhiggins@gmail.com>
2021-08-27 16:45:05 -0600

    Extract SearchModalTrigger

--- packages/app/lib/components/Home.jsx
+++ packages/app/lib/components/Home.jsx
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Pokemon } from '../modules/pokemon';
-import SearchModal from './SearchModal';
-
-import Modal from 'react-modal';
+import SearchModalTrigger from './SearchModalTrigger';
 
 const SearchButton = () => {
   return(
@@ -13,20 +11,6 @@ const SearchButton = () => {
 
 const Home = () => {
   const [pokemon, setPokemon] = useState("pikachu");
-  const [modalIsOpen, setIsOpen] = useState(false);
-
-  function openModal() {
-    setIsOpen(true);
-  }
-
-  function afterOpenModal() {
-    // references are now sync'd and can be accessed.
-    subtitle.style.color = '#f00';
-  }
-
-  function closeModal() {
-    setIsOpen(false);
-  }
 
   useEffect(() => {
     Pokemon.card.find('base1-4')
@@ -37,15 +21,8 @@ const Home = () => {
   
   return(
     <div>
-      <button onClick={openModal}>Open Modal</button>
-      <Modal
-        isOpen={modalIsOpen}
-        onAfterOpen={afterOpenModal}
-        onRequestClose={closeModal}
-        contentLabel="Example Modal"
-      >
-        <SearchModal />
-      </Modal>
+      <SearchModalTrigger />
+      <div>Other Content</div>
     </div>
   );
 }
